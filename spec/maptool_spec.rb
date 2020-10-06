a5919d36777c44bbba8145f6aa1c19c5aef3fe38
Elliott HEBERT <ehebert@LAPTOP-12T1LJI3.localdomain>
2020-10-06 10:26:33 +0200

    add default gem files

---
+++ spec/maptool_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Maptool do
+  it "has a version number" do
+    expect(Maptool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
